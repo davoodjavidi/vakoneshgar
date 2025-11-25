4381d591733d643365919921e00158cf0c9dc8aa
davood javidi <javididavid1993@gmail.com>
2025-11-25 21:12:56 +0330

    start validate login form

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Activity } from "lucide-react";
+import Link from "next/link";
+
 export default function LoginPage() {
-  return <div>صفحه ورود</div>;
+  return (
+    <>
+      <div className="flex gap-2 items-center">
+        <Activity className="text-primary" size={30} />
+        <h3>واکنشگرا</h3>
+      </div>
+
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>ورود</CardTitle>
+          <CardDescription>ورود به حساب کاربری</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>فرم ورود</p>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>حساب کاربری ندارید؟</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">ثبت نام</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
